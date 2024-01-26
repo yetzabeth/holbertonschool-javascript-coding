1d794d51a774b752d024a47cd35b4df38f6716e7
yetzabeth <135637883+yetzabeth@users.noreply.github.com>
2024-01-26 22:20:40 +0000

    tansk 1

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -2,24 +2,10 @@
 
 const fs = require('fs');
 
-// Get the file path and string to write from command line arguments
-const filePath = process.argv[2];
-const contentToWrite = process.argv[3];
+const [, , filePath, strToWrite] = process.argv;
 
-// Check if both file path and content are provided
-if (!filePath || !contentToWrite) {
-  console.error('Usage: ./1-writeme.js <file-path> <string-to-write>');
-  process.exit(1);
-}
-
-// Write the content to the file in utf-8
-fs.writeFile(filePath, contentToWrite, 'utf-8', (err) => {
+fs.writeFile(filePath, strToWrite, 'utf-8', (err) => {
   if (err) {
-    // Print the error object if an error occurred during writing
-    console.error(err);
-    process.exit(1);
+    console.log(err);
   }
-
-  // Print the content of the file without additional messages
-  console.log(contentToWrite);
 });
